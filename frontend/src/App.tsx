18582871847ae393c38ba5d46b2624b642e42d4a
Silva <tiago.g.silva@accenture.com>
2021-09-07 09:35:53 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,31 @@
 import NavBar from './components/navbar/index'
 import Footer from './components/footer/index'
 import DataTable from './components/dataTable/index'
+import BarChart from './components/barChart/index'
+import DonutChart from './components/donutChart/index'
 
 function App() {
   return (
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">DSVendas</h1>
+        <h1 className="text-primary py-3">DSVendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas as Vendas</h2>
+        </div>
         <DataTable />
       </div>
       <Footer />
-
     </>
   );
 }

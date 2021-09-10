1bc00d234e4fe53ad44d2721f87ee8968d986094
Silva <tiago.g.silva@accenture.com>
2021-09-10 12:04:49 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,24 @@
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+import { Link } from "react-router-dom";
 
 
 export default function Home() {
     return (
-        <div>
-            <h1>Página de Home</h1>
-        </div>  
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                </div>
+                <Link className="btn btn-primary btn-large"to='/dashboard'>
+                    Acessar Dashboard
+                </Link>
+            </div>
+            <Footer />
+        </>
     );
 }

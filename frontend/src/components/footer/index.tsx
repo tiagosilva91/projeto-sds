b291f9eba384abc920ffa115a1d9624e1e8309cf
Silva <tiago.g.silva@accenture.com>
2021-09-11 08:03:00 -0300

    Add Linkedin

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/tiagosilva91" target="_blank" rel="noreferrer">Tiago Silva</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/tiago-silva-9242a0204/" target="_blank" rel="noreferrer">Tiago Silva</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
